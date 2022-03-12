c22b11d43fe062799657878b7d7c68392d6c6ade
Kai Lyons <kai@potabi.com>
2022-03-12 10:33:55 -0700

    Create basic if/else functionality

--- future/if-else.oxa.py
+++
@@ -1,7 +0,0 @@
-var_one=1
-var_two=2
-var_one==var_two:
-	print("Equals")
-else:
-	:
-	print("Not_equals")
--- oxa_python_codegen.py
+++ oxa_python_codegen.py
@@ -22,11 +22,11 @@ def oxa_py3_codegen(file):
         elif i == "]":
             i = i.replace("]", ")")
             python_codegen.append(i)
-        elif i.endswith("{") and not i.startswith("}"):
+        elif i.endswith("{"):
             i = i.replace("{", ":\n\t")
             python_codegen.append(i)
-        elif i == "}{":
-            i = i.replace("}{", "else:\n\t")
+        elif i == "}" and file[file.index(i) + 1] =="{":
+            i = i.replace("}", "else")
             python_codegen.append(i)
         else:
             python_codegen.append(i)
@@ -70,13 +70,25 @@ def oxa_py3_codegen(file):
             x = x.replace(",-","-")
             python_printline[python_printline.index(x)] = x.replace("-,","-")
             x = x.replace("-,","-")
-            
-            
 
-    # Codegen Stage 3: Python final file
+    # Stage 3: Create elif
+    python_ifelse = []
+    for i in python_printline:
+        if i == "==":
+            i = "if " + python_printline[python_printline.index(i) - 1] + " == " + python_printline[python_printline.index(i) + 1] + ":"
+            python_ifelse.append(i)
+        else:
+            python_ifelse.append(i)
+    
+    # Stage 4: Clean if statement
+    for i in python_ifelse:
+        if "if " in i:
+            python_ifelse[python_ifelse.index(i)-1]=""
+
+    # Codegen Stage 5: Python final file
     python_file = []
     python_line = ''
-    for i in python_printline:
+    for i in python_ifelse:
         if i != ";":
             python_line = python_line + i
         elif i == ";":
